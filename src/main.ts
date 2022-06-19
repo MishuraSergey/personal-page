5be73cf5699f4fef4ec8583076ab8ea17611b6d7
Serhii Mishura <dev.mishura@gmail.com>
2022-06-18 19:18:06 -0600

    home page added, minor style fixes

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,9 @@ const NAV: HTMLElement = document.getElementById('main_nav'),
 ['DOMContentLoaded', 'scroll'].forEach(event => {
     document.addEventListener(event, function(): void {
         let posY: number = window.scrollY;
+
         posY > 0 ? NAV.classList.add('scrolled') : NAV.classList.remove('scrolled');
+
         posY > window.innerHeight ? UP_BUTTON.classList.add('shown') : UP_BUTTON.classList.remove('shown');
     })
 });
@@ -28,12 +30,27 @@ UP_BUTTON.addEventListener('click', function () : void {
 });
 
 CONTENT_LINKS.forEach(el => {
-    el.onclick = () => {
+    el.onclick = (event:MouseEvent) => {
+        event.preventDefault();
+
         const ELID: string = el.getAttribute('href'),
             ACTIVE: HTMLElement = document.querySelector(ELID),
             SIBLINGS: Array<Element> = Array.from(ACTIVE.parentElement.children).filter(ch=>ch!=ACTIVE);
+
         ACTIVE.classList.add('active');
         SIBLINGS.forEach(sibling => sibling.classList.remove('active'));
+
+        Array.from(MOBILE_NAV.children).forEach(child => {
+            child.classList.remove('active');
+            if (child.getAttribute('href') === ELID){
+                child.classList.add('active');
+            }
+        });
+
+        //close float nav and set burger to initial position on mobile devices
+        if (MOBILE_NAV.classList.contains('enabled')){
+            [BURGER_BUTTON, MOBILE_NAV].forEach(el => el.classList.remove('enabled'));
+        }
     }
 })
 
